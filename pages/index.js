440b2a36dd1a9fc4ae3d70300ba0796c27c97d57
toygarazar <82677526+toygarazar@users.noreply.github.com>
2022-04-13 16:50:16 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,3 @@
+<title>Kampanya</title>
+<h1>Giriş</h1>
 <p><a href="https://www.penimajor.com.tr"><img src="https://imgyukle.com/f/2022/04/13/RJdVD8.png" alt="" width="1000" height="685" /></a></p>
